48a71307bd872099b99118f9c54971eb0e7d2fe1
ashiqur12315 <ashiqur12315@gmial.com>
2024-05-15 21:41:09 +0530

    Implemented search job query

--- index.js
+++ index.js
@@ -210,17 +210,7 @@ async function run() {
         // })
         // app.post('/jwt', (req, res) => {
         //   const user = req.body;
-        //   console.log(user)
-        //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-        //   res
-        //   .cookie('token', token, {
-        //     httpOnly: true,
-        //     secure: true,
-        //     sameSite: 'none'
-        //   })
-        //   .send({success: true})
-        // })
-
+       
 
        
 
